3b8b3d5fe96e981697555e6e6c9d5eb793ff9d55
salemking22 <brynjerel@gmail.com>
2025-06-10 22:28:32 +0200

    Completed the Tracking Program Assignment

---
+++ week07/ExerciseTracking/Cycling.cs
@@ -0,0 +1,16 @@
+public class Cycling : Activity
+{
+    private double _speed; // in kilometers per hour
+
+    public Cycling(DateTime date, int minutes, double speed)
+        : base(date, minutes)
+    {
+        _speed = speed;
+    }
+
+    public override double GetSpeed() => _speed;
+
+    public override double GetDistance() => (_speed * GetMinutes()) / 60;
+
+    public override double GetPace() => 60 / _speed;
+}
